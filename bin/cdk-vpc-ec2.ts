c2fe172322e8de7436d7dbcf2271845431ff0b19
nnydtmg <nnydtmg@gmail.com>
2023-02-09 22:56:48 +0900

    first commit

--- bin/cdk-vpc-ec2.ts
+++ bin/cdk-vpc-ec2.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { CdkVpcEc2Stack } from '../lib/cdk-vpc-ec2-stack';
+import { DefaultStackSynthesizer } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-new CdkVpcEc2Stack(app, 'CdkVpcEc2Stack');
+new CdkVpcEc2Stack(app, 'CdkVpcEc2Stack', {
+    synthesizer: new DefaultStackSynthesizer({
+        fileAssetsBucketName: 'cdk-vpc-ec2-assets', // bootstrapのバケット名を指定
+    }),
+    description: 'This is a cdk-vpc-ec2-sample-stack', // CloudFormationスタックに説明を追加
+});
